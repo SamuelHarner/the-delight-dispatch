35cdef79b31941ad9165f7a6495e1a0ad2e402f7
Samuel Härner <58339914+SamuelHarner@users.noreply.github.com>
2024-04-23 15:24:49 +0200

    Change data version

--- graph_generation.py
+++ graph_generation.py
@@ -12,7 +12,7 @@ def get_sentiment_history():
     project = hopsworks.login()
     fs = project.get_feature_store()
 
-    most_positive_fg = fs.get_feature_group(name="articles_most_positive", version=3)
+    most_positive_fg = fs.get_feature_group(name="articles_most_positive", version=1)
     most_positive_df = most_positive_fg.read()
     most_positive_sentiment = most_positive_df["sentiment"].values
     most_positive_date = most_positive_df["pubdate"].values
